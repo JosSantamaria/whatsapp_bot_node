1b08c8a04e1425b4c7b7befca7acc0948666677c
Jos Santa <joset.sistemas@gmail.com>
2022-06-08 16:19:10 -0500

    WhatsApp_Bot_Node

---
+++ config/mysql.js
@@ -0,0 +1,18 @@
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : process.env.SQL_HOST || 'localhost',
+  user     : process.env.SQL_USER || 'me',
+  password : process.env.SQL_PASS || '',
+  database : process.env.SQL_DATABASE || 'my_db'
+});
+
+const connect = () => connection.connect(function(err) {
+    if (err) {
+        console.error('Error de Conexion: ' + err.stack);
+        return;
+    }
+
+    console.log('Conexion correcta con tu base de datos MySQL')
+});
+
+module.exports = {connect, connection}
